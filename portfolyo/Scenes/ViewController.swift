bf9278dbdd7b1582d3fee949f855c9b3d9639a00
eren-celik <elik.dark91@gmail.com>
2022-06-06 00:10:33 +0300

    initial commit

---
+++ portfolyo/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  portfolyo
+//
+//  Created by Eren  Çelik on 6.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+

71cdfb6a13c83e6349cc8f6f83ebad9d64b73730
eren-celik <elik.dark91@gmail.com>
2022-06-14 22:54:12 +0300

    wallet view added

---
+++ portfolyo/Scenes/Wallet/Resources/Wallet+Contracts.swift
@@ -0,0 +1,8 @@
+//
+//  Wallet+Contracts.swift
+//  portfolyo
+//
+//  Created by Eren  Çelik on 14.06.2022.
+//
+
+import Foundation
---
+++ portfolyo/Scenes/Wallet/View/WalletViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WalletViewController.swift
+//  portfolyo
+//
+//  Created by Eren  Çelik on 14.06.2022.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
